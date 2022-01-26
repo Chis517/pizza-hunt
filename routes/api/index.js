a4b26b31a5641164827a53d933211c84174eaff4
Chice <cko517@hotmail.com>
2022-01-26 01:41:02 -0500

    Integrate with Front End

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const commentController = require('../../controllers/comment-controller');
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+// adds prefix of `/comments` to routes created from `comment-routes.js`
+router.use('/comments', commentRoutes);
 // adds prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
